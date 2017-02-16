37c4c802c64e141bff78ce2522994b63d39e7150
chenqin <41327518@qq.com>
2017-02-16 12:43:11 +0800

    add rest

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -81,7 +81,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
 
     if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
+      redirect_to root_path, alert: "拥有者才可以编辑修改"
     end
   end
 end
